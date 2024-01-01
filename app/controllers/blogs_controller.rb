aab2eeef126f61d77c54a29c4616b65b1c0ba8bd
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-01-01 21:47:52 +0900

    通常ユーザがアイキャッチ画像を設定できないよう修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,8 +16,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @blog = current_user.blogs.new(blog_params)
@@ -54,6 +53,10 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    if current_user.premium
+      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    else
+      params.require(:blog).permit(:title, :content, :secret)
+    end
   end
 end
